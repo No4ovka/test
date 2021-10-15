1b8661e76d27f8ea2a24717c709701e6d07026e0
No4ovka <92082996+No4ovka@users.noreply.github.com>
2021-10-15 09:56:49 +0300

    Update middle.h

--- middle.h
+++ middle.h
@@ -6,7 +6,7 @@ using namespace std;
 
 void itc_num_print(int num1);
 int itc_len_num (long long num1);
-int itc_sum_num(long long num1);
+/*int itc_sum_num(long long num1);
 long long itc_multi_num(long long num1);
 int itc_max_num(long long num1);
 int itc_min_num(long long num1);
@@ -26,6 +26,6 @@ int itc_abs(int num1);
 double itc_pow(int n, int i);
 int perevorot(int num1);
 long long modyl(long long num1);
-long long dlinna(long long num1);
+long long dlinna(long long num1);*/
 
 #endif // MIDDLE_H_INCLUDED
