1f464e809981173e1e613e6f588aec900708a083
No4ovka <92082996+No4ovka@users.noreply.github.com>
2021-10-15 10:38:57 +0300

    Update middle.h

--- middle.h
+++ middle.h
@@ -16,9 +16,9 @@ bool itc_mirror_num(long long num1);
 int itc_mirror_count(long long num1);
 int itc_second_max_num(long long num1);
 int itc_second_simple_max_num(long long num1);
-/*long long itc_bin_num(long long num1);
+long long itc_bin_num(long long num1);
 long long itc_oct_num(long long num1);
-int itc_rev_bin_num(long long num1);
+/*int itc_rev_bin_num(long long num1);
 int itc_rev_oct_num(long long num1);
 int itc_covert_num(long long num1, int ss);
 int itc_rev_covert_num(long long num1, int ss);*/
